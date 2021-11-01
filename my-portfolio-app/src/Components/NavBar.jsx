3d75886a3c60c09e544081e5277e646601027268
lzalit238 <lzalit238@west-mec.org>
2021-11-01 16:13:35 -0700

    Update NavBar.jsx

--- my-portfolio-app/src/Components/NavBar.jsx
+++ my-portfolio-app/src/Components/NavBar.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <nav>
+            </nav>
+        </>
     )
 }
 
